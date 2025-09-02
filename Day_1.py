ede8de204ad6cd408d263b6da49af92216b44092
unknown <sumayaaktertamim@gmail.com>
2025-09-02 11:50:50 +0600

    initial commit

---
+++ Day_1.py
@@ -0,0 +1,18 @@
+# w= int(input())
+ 
+# if w==2:
+#     print ('No')
+# elif w%2==0:
+#     print ('Yes')
+# else :
+#     print ('No')
+    
+#Type-2
+
+w= int(input())
+if w%2==0 and w>2:
+        print ("Yes")
+else :
+    print ("No")
+    
+                                                                                                                                                          
---
+++ Day_2.py
@@ -0,0 +1,3 @@
+M, N=map(int,(input().split()))
+number_of_Domino= (M*N)//2
+print (number_of_Domino)
