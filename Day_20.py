0739f94b2ff040f39b664a5763ba4c006166ff17
unknown <sumayaaktertamim@gmail.com>
2025-09-21 23:19:29 +0600

    New Challenge

--- Day_20.py
+++ Day_20.py
@@ -8,3 +8,6 @@ for i in range(n):
     
 print(count)
     
+    
+    
+    
---
+++ Day_21.py
@@ -0,0 +1,16 @@
+# Drinks-800
+n= int(input())
+x=list(map(int,input().split()))
+    total=(sum(x))/n
+print(total)
+
+# type-2
+
+n = int(input())                     
+x = list(map(int, input().split()))  
+total = 0                           
+for i in range(n):                   
+    total += x[i]                    
+
+Mean = total / n                  
+print(Mean)
