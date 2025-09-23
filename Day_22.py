c1bdcfd51048c10a5d09e4109c49527bb3879ef1
unknown <sumayaaktertamim@gmail.com>
2025-09-23 23:16:54 +0600

    New Challenge

--- Day_22.py
+++ Day_22.py
@@ -1,8 +1,9 @@
 # presents-800
+ 
 n = int(input())
 g = list(map(int, input().split()))
 
-for i in range(1, n+1)
+for i in range(1, n+1):
     for j in range(n):  
         if g[j] == i:     
-    print(j+1, sep=" ")  
+            print(j+1, end=" ")
---
+++ Day_23.py
@@ -0,0 +1,10 @@
+# hulk-800
+n=int(input())
+word=[]
+for i in range(1,n+1):
+    if i%2!=0:
+        word.append ("I hate") 
+    else:
+        word.append("I love")
+print(" that ".join(word),"it")   
+    
