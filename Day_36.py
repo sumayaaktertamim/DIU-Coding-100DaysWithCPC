d4192145363d6bcd472b13744b35624750e73224
unknown <sumayaaktertamim@gmail.com>
2025-10-06 04:22:54 +0600

    New Challenge

--- Day_36.py
+++ Day_36.py
@@ -1,3 +1,5 @@
+# A. Game With Sticks-900
+# type-1
 n,m=map(int,input().split())
 if n>m:
     count=m
@@ -11,3 +13,11 @@ else:
         print("Malvika")
     else:
         print("Akshat")
+# type-2
+    
+n,m=map(int,input().split())
+count=min(n,m)
+if count%2==0:
+    print("Malvika")
+else:
+    print("Akshat")
